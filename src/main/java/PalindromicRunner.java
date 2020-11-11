7ec00d6a233af682963f9954ef7671a35acc0b14
Jared Zutt <645681@aacps.org>
2020-11-11 17:52:26 +0000

    Complete PalindromicNumberFinder.

--- src/main/java/PalindromicRunner.java
+++ src/main/java/PalindromicRunner.java
@@ -1,6 +1,5 @@
 public class PalindromicRunner {
     public static void main(String[] args) {
-        // be sure you're testing each method as you work then produce your final output using the toString method
-
+        System.out.println(new PalindromicNumberFinder(72927));
     }
 }
